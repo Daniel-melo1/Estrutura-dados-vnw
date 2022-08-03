23bb31e65153bb71c06f1d125a7ce05f973f8482
Daniel Melo <104798383+Daniel-melo1@users.noreply.github.com>
2022-08-03 14:19:38 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Crie um array que receba 5 itens e exiba no console.
+let dados = ["Daniel", "Lucas", "Jhony", "Luana"];
+console.log(dados);
+
+// Utilize um método para adicionar um nome ao inicio do array.
+dados.unshift("Eduarda");
+console.log(dados);
+
+// Utilize um método para remover o último nome do array.
+dados.pop();
+console.log(dados);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+dados.push("Joana", "Ilda");
+console.log(dados);
+
+// Utilize um método para remover o primeiro nome do array.
+dados.shift();
+console.log(dados);
+
+let number = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+console.log(number.sort());
